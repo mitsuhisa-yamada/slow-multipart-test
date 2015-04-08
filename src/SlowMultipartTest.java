f21a0671f638fba0fcf5bd6a6e98852fd66e33dd
山田　満久 <miyamada@yahoo-corp.jp>
2015-04-08 16:01:19 +0900

    回数の指定を削除

--- src/SlowMultipartTest.java
+++ src/SlowMultipartTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -26,7 +27,6 @@ public class SlowMultipartTest implements Runnable {
     private static final String NAME = "file";
     private static final String DEFAULT_SLEEP_MS = "100";
     private static final String DEFAULT_BUFFER_BYTES = "1024";
-    private static final String DEFAULT_REQUEST_COUNT = "1";
     private static final String DEFAULT_TEST_TIME_SEC = "0";
     private static final String DEFAULT_PARALLELISM = "1";
     private static final String DEFAULT_OUTPUT_INTERVAL_SEC = "1";
@@ -38,7 +38,6 @@ public class SlowMultipartTest implements Runnable {
         options.addOption("n", "minsleep", true, "Min sleep (ms). default: " + DEFAULT_SLEEP_MS);
         options.addOption("b", "buffer", true, "Buffer size (Bytes). defult: " + DEFAULT_BUFFER_BYTES);
         options.addOption("l", "list", true, "File list (line by line text file)");
-        options.addOption("c", "count", true, "Request count. default: " + DEFAULT_REQUEST_COUNT);
         options.addOption("t", "time", true, "Test time (sec). default: " + DEFAULT_TEST_TIME_SEC);
         options.addOption("p", "parallelism", true, "Parallelism. default: " + DEFAULT_PARALLELISM);
         options.addOption("i", "interval", true, "Output interval (sec). default: " + DEFAULT_OUTPUT_INTERVAL_SEC);
@@ -53,7 +52,6 @@ public class SlowMultipartTest implements Runnable {
             test.minSleepMs = Long.parseLong(commandLine.getOptionValue("n", DEFAULT_SLEEP_MS));
             test.bufferBytes = Integer.parseInt(commandLine.getOptionValue("b", DEFAULT_BUFFER_BYTES));
             test.fileNameList = parseFileNameList(commandLine.getOptionValue("l"));
-            test.requestCount = Integer.parseInt(commandLine.getOptionValue("c", DEFAULT_REQUEST_COUNT));
             test.testTimeMs = TimeUnit.SECONDS.toMillis(Long.parseLong(commandLine.getOptionValue("t", DEFAULT_TEST_TIME_SEC)));
             test.parallelism = Math.max(1, Integer.parseInt(commandLine.getOptionValue("p", DEFAULT_PARALLELISM)));
             test.outputIntervalMs = TimeUnit.SECONDS.toMillis(Long.parseLong(commandLine.getOptionValue("i", DEFAULT_OUTPUT_INTERVAL_SEC)));
@@ -80,16 +78,12 @@ public class SlowMultipartTest implements Runnable {
             return;
         }
         int size = fileNameList.size();
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < requestCount || System.currentTimeMillis() - start < testTimeMs; i++) {
+        while (System.currentTimeMillis() - startTime < testTimeMs) {
             if (postFile(fileNameList.get((int)(size * Math.random())))) {
                 synchronized(okCount) {
                     okCount++;
                 }
             }
-        }
-        synchronized(activeThreadCount) {
-            activeThreadCount--;
         }
     }
 
@@ -102,22 +96,23 @@ public class SlowMultipartTest implements Runnable {
             startTime = System.currentTimeMillis();
             for (int i = 0; i < parallelism; i++) {
                 threadList.get(i).start();
-                activeThreadCount++;
             }
             if (outputIntervalMs > 0) {
                 Thread.sleep(outputIntervalMs);
-                while (activeThreadCount > 0) {
+                while (System.currentTimeMillis() - startTime < testTimeMs) {
                     outputTimeRecord(false);
                     Thread.sleep(outputIntervalMs);
                 }
             }
+            outputTimeRecord(true);
+            System.out.print("Terminating...");
             for (int i = 0; i < parallelism; i++) {
                 threadList.get(i).join();
             }
+            System.out.println("Finished!");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        outputTimeRecord(true);
     }
 
     private boolean postFile(String fileName) {
@@ -153,6 +148,10 @@ public class SlowMultipartTest implements Runnable {
             char[] buffer = new char[bufferBytes];
             int len;
             while ((len = reader.read(buffer, 0, bufferBytes)) >= 0) {
+                if (System.currentTimeMillis() - startTime >= testTimeMs) {
+                    reader.close();
+                    throw new TimeoutException();
+                }
                 outputStream.write(String.valueOf(buffer).getBytes(), 0, len);
                 outputStream.flush();
                 Thread.sleep(sleepMs);
@@ -174,7 +173,7 @@ public class SlowMultipartTest implements Runnable {
             synchronized(timeRecorder) {
                 timeRecorder.record(finish - start, clientSleep, serverSleep);
             }
-        } catch (SocketException e) {
+        } catch (SocketException | TimeoutException e) {
             // ignore
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -209,19 +208,16 @@ public class SlowMultipartTest implements Runnable {
                         parallelism, count, okCount, time, qps);
             } else {
                 double qps = (count - prevCount) / (time - prevTime);
-                System.out.printf("Active Threads: %d, Count: %d (OK: %d), Time: %ds, Current QPS: %f\n",
-                        activeThreadCount, count, okCount, (int)time, qps);
+                System.out.printf("Parallelism: %d, Count: %d (OK: %d), Time: %ds, Current QPS: %f\n",
+                        parallelism, count, okCount, (int)time, qps);
             }
-            System.out.printf("Avg: %dms (non-sleep: %dms), Max: %dms (non-sleep: %dms), Min: %dms (non-sleep: %dms)\n",
+            System.out.printf("Avg: %dms (non-sleep: %dms), Max: %dms (non-sleep: %dms), Min: %dms (non-sleep: %dms)\n\n",
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getTotalAvg()),
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getActiveAvg()),
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getTotalMax()),
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getActiveMax()),
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getTotalMin()),
                     TimeUnit.NANOSECONDS.toMillis(timeRecorder.getActiveMin()));
-        }
-        if (!finish) {
-            System.out.println();
         }
         System.out.flush();
         prevCount = count;
@@ -276,12 +272,10 @@ public class SlowMultipartTest implements Runnable {
     private long minSleepMs = 0;
     private int bufferBytes = 0;
     private List<String> fileNameList = null;
-    private int requestCount = 0;
     private long testTimeMs = 0;
     private int parallelism = 0;
     private long outputIntervalMs = 0;
     private long startTime = 0;
     private TimeRecorder timeRecorder = new TimeRecorder();
     private Integer okCount = 0;
-    private Integer activeThreadCount = 0;
 }
